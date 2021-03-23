862b355995180f06658716bccebf9dac46453327
huxinche <70772950+huxinche@users.noreply.github.com>
2021-03-23 14:53:52 -0400

    Update AsteroidCollision.java

--- AsteroidCollision.java
+++ AsteroidCollision.java
@@ -1,5 +1,37 @@
-package Leetcode;
-
-public class AsteroidCollision {
-
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < asteroids.length; i++) {
+            if (stack.isEmpty() || asteroids[i] > 0) {
+                stack.push(asteroids[i]);
+                continue;
+            }
+            while (true) {
+                int prev = stack.peek();
+                if (prev < 0) {
+                    stack.push(asteroids[i]);
+                    break;
+                }
+                if (prev == -asteroids[i]) {
+                    stack.pop();
+                    break;
+                }
+                if (prev > -asteroids[i]) {
+                    break;
+                }
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(asteroids[i]);
+                    break;
+                }
+            }
+        }
+        int size=stack.size();
+        int[] result = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
 }
+
